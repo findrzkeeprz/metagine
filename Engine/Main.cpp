8de74441058d11c351e98ff3b2376257c4edd9f7
James Munro <audiocheese@gmail.com>
2008-12-11 22:45:14 +0000

    Improved engine initialization code.

--- Engine/Main.cpp
+++ Engine/Main.cpp
@@ -3,11 +3,22 @@
 
 int main( int argc, char *argv[] )
 {
+    IEngine* pEngine = NULL;
+    
+    printf(" -> Starting Metagine.\n");
+    
+    if( !( pEngine = Engine::GetInstance() ) ) {
+        printf(" >! ERROR creating CEngine object.\n");
+        return 0;
+    }
+    
     // Start the main engine.
-    if( !Engine::GetInstance()->Init() ) {
+    if( !pEngine->Init() ) {
         printf("Something has gone terribly wrong!\n");
         return 0;
-    }    
+    }
+    
+    delete pEngine;
     
     return 0;
 }
