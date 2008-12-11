f0e1bdff086c985bc2663ad15782746623625cac
James Munro <audiocheese@gmail.com>
2008-12-11 19:10:43 +0000

    Added main engine initialization code.

--- Engine/Main.cpp
+++ Engine/Main.cpp
@@ -1,6 +1,13 @@
 #include <stdio.h>
+#include "CEngine.h"
 
 int main( int argc, char *argv[] )
 {
+    // Start the main engine.
+    if( !Engine::GetInstance()->Init() ) {
+        printf("Something has gone terribly wrong!\n");
+        return 0;
+    }    
+    
     return 0;
 }
