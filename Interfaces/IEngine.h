671a4ba026977e4d5616cfd2716e9f3cb5b3125b
James Munro <audiocheese@gmail.com>
2008-12-11 19:12:34 +0000

    Created basic engine interface class.

---
+++ Interfaces/IEngine.h
@@ -0,0 +1,38 @@
+// This file is part of Metagine.
+// 
+// Metagine is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Metagine is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Metagine.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef _IENGINE_H_
+#define _IENGINE_H_
+
+#include "../Interfaces/IVarManager.h"
+
+class IEngine
+{
+public:
+    
+    /// Virtual destructor method.
+    virtual ~IEngine( void ) { };
+    
+    /// Virtual function to initialize the main engine functionality.
+    virtual bool Init( void ) = 0;
+    
+    /// Virtual function to initialize the main engine functionality.
+    virtual void Shutdown( void ) = 0;
+    
+    /// Virtual accessor to get a pointer to the variable manager.
+    virtual IVarManager* GetVarManager( void ) = 0;
+};
+
+#endif // _IENGINE_H_
