24b9451314e53695b6aa2aa4e76e360539eabdfa
James Munro <audiocheese@gmail.com>
2008-12-15 08:14:38 +0000

    Added a new base interface class to derive all major class interfaces from. This is not for smaller objects such as client variables, but rather the big players such as the variable manager. The main engine should store a list of all registered interfaces, accessible via name string.

---
+++ Interfaces/IBaseInterface.h
@@ -0,0 +1,33 @@
+// This file is part of Metagine.
+// 
+// Metagine is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Metagine is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Metagine.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef _IBASEINTERFACE_H_
+#define _IBASEINTERFACE_H_
+
+class IBaseInterface
+{
+public:
+    
+    /// Virtual destructor method.
+    virtual ~IBaseInterface( void ) { };
+    
+    /// Virtual function to get the interface's name.
+    virtual const char* GetName( void );
+
+    /// Virtual function to get the interface's version string.
+    virtual const char* GetVersion( void );
+};
+
+#endif // _IBASEINTERFACE_H_
