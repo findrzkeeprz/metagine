101618b0fb0fa86ff042eaaf4f37587b80e4a8e6
James Munro <audiocheese@gmail.com>
2009-03-28 18:58:28 +0000

    New game module added to the project. Added a header defining an EngineExports_t structure. Game logic code should be implemented in the game module and not the main engine.

---
+++ Game/Main.cpp
@@ -0,0 +1,59 @@
+// This file is part of Metagine.
+// 
+// Metagine is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Metagine is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Metagine.  If not, see <http://www.gnu.org/licenses/>.
+
+#include <windows.h>
+#include <stdio.h>
+#include "Game86.h"
+#include "../Public/Exports.h"
+
+EngineExports_t Exports;
+
+extern "C" GAME86_API bool InitGameModule( IEngine* pEngine )
+{
+	// Show-stopping error.
+	if( !pEngine ) {
+		printf(" -! ERROR null MEngine interface pointer.\n");
+		return false;
+	}
+
+	printf(" -> InitGameModule() received engine interface pointer: 0x%X.\n",pEngine);
+
+	// Grab the interface exports.
+	if( !( Exports.pVarManager = (IVarManager*)pEngine->GetInterfaceByName("MVarManager") ) ) {
+		printf(" -! ERROR null MVarManager interface pointer.\n");
+		return false;
+	} else if( !( Exports.pRenderer = (IRenderer*)pEngine->GetInterfaceByName("MRenderer") ) ) {
+		printf(" -! ERROR null MRenderer interface pointer.\n");
+		return false;
+	}
+
+	return true;
+}
+
+BOOL APIENTRY DllMain( HMODULE hModule,
+                       DWORD  ul_reason_for_call,
+                       LPVOID lpReserved
+					 )
+{
+	switch (ul_reason_for_call)
+	{
+	case DLL_PROCESS_ATTACH:
+	case DLL_THREAD_ATTACH:
+	case DLL_THREAD_DETACH:
+	case DLL_PROCESS_DETACH:
+		break;
+	}
+	return TRUE;
+}
